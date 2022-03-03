428cb205be30ba46405f442e3cd408bdedb68686
D0ugg <douglasmr2011@hotmail.com>
2022-03-03 20:53:55 -0300

    first commit

---
+++ lampada.js
@@ -0,0 +1,47 @@
+const turnOnOff = document.getElementById ( 'turnOnOff' );
+const lamp = document.getElementById ( 'lamp' );
+
+function isLampBroken () {
+    return lamp.src.indexOf ( 'quebrada' ) > -1
+}
+
+function isLampBrokenOff () {
+    return turnOnOff.textContent = 'Ops, quebrou!' 
+
+}
+
+function lampOn () {
+    if ( !isLampBroken () ) 
+    {
+        lamp.src = './img/ligada.jpg';
+    }
+}
+
+function lampOff () {
+    if ( !isLampBroken () ) 
+    {
+        lamp.src = './img/desligada.jpg';
+    }
+}
+
+function lampBroken () {
+    if ( isLampBrokenOff () )
+    lamp.src = './img/quebrada.jpg';
+}
+
+function lampOnOff () {
+    if ( turnOnOff.textContent == 'Ligar' ) {
+        lampOn();
+        turnOnOff.textContent = 'Desligar';
+    }else{
+        lampOff();
+        turnOnOff.textContent = 'Ligar';
+    }
+}
+
+
+turnOnOff.addEventListener ( 'click', lampOnOff );
+
+lamp.addEventListener ( 'mouseover', lampOn );
+lamp.addEventListener ( 'mouseleave', lampOff );
+lamp.addEventListener ( 'dblclick', lampBroken );
